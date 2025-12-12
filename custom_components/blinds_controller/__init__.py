ee23812ac109afe47b2d564b095a009b8f99e019
schelli99 <101871899+schelli99@users.noreply.github.com>
2025-12-12 18:19:55 +0100

    Fix for newer ha versions

--- custom_components/blinds_controller/__init__.py
+++ custom_components/blinds_controller/__init__.py
@@ -1,25 +1,25 @@
-# Import necessary modules from Home Assistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-# Import the domain constant from the current package
 from .const import DOMAIN
 
+PLATFORMS = ["cover"]
+
 async def async_setup(hass: HomeAssistant, config: dict):
-    # Return True here and the user will be able to initiate the config flow from the integrations page
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    # Set up your integration with the configuration entry
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry.data
-    # Load the cover platform with the configuration entry
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "cover")
-    )
+
+    # Wichtig: in neuen HA-Versionen direkt awaiten
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    # Unload your integration when the configuration entry is removed
-    await hass.config_entries.async_forward_entry_unload(entry, "cover")
-    hass.data[DOMAIN].pop(entry.entry_id)
-    return True
+    # Neues API zum Unload
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id, None)
+
+    return unload_ok
