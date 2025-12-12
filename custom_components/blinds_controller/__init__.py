2aacbf04a373669ac9d36a8243e4d85cc283b064
schelli99 <101871899+schelli99@users.noreply.github.com>
2025-12-12 18:20:46 +0100

    Update __init__.py

--- custom_components/blinds_controller/__init__.py
+++ custom_components/blinds_controller/__init__.py
@@ -11,12 +11,10 @@ async def async_setup(hass: HomeAssistant, config: dict):
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry.data
 
-    # Wichtig: in neuen HA-Versionen direkt awaiten
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    # Neues API zum Unload
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
     if unload_ok:
